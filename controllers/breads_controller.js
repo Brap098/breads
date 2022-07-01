df1924f5c783629a8086887fd0059ff483e371eb
Brap098 <Brapagon.Work@gmail.com>
2022-06-30 21:28:25 -0400

    fixed

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -5,9 +5,15 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-    res.render('index')
-  // res.send(Bread)
+  res.render('index',
+    {
+      breads: Bread,
+      title: 'Index Page'
+    }
+  )
 })
+
+
 
 
 // SHOW
